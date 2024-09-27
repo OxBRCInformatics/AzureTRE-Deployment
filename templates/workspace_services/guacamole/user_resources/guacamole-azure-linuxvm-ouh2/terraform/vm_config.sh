9b83cd9795dc4a1f14a121362f1385b7ccc9b41b
Jaimie Withers <jaimie.withers@bdi.ox.ac.uk>
2024-09-27 07:36:05 +0000

    test source linux build

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm-ouh2/terraform/vm_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm-ouh2/terraform/vm_config.sh
@@ -2,75 +2,81 @@
 
 set -o errexit
 set -o pipefail
-# set -o nounset
+set -o nounset
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
-# set -o xtrace
+set -o xtrace
 
 # Remove apt sources not included in sources.list file
 sudo rm -f /etc/apt/sources.list.d/*
 
-IF [ "${INSTALL_UI}" -eq 1 ]; then
-  echo "init_vm.sh: START"
-  sudo apt update || true
-  sudo apt upgrade -y
-  sudo apt install -y gnupg2 software-properties-common apt-transport-https wget dirmngr gdebi-core
-  sudo apt-get update || true
+# Update apt packages from configured Nexus sources
+echo "init_vm.sh: START"
+sudo apt update || true
+sudo apt upgrade -y
+sudo apt install -y gnupg2 software-properties-common apt-transport-https wget dirmngr gdebi-core
+sudo apt-get update || true
 
-  ## Desktop
-  echo "init_vm.sh: Desktop"
-  sudo systemctl start gdm3 || true
-  DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure gdm3 || true
-  sudo apt install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-  echo /usr/sbin/gdm3 > /etc/X11/default-display-manager
+## Desktop
+echo "init_vm.sh: Desktop"
+sudo systemctl start gdm3 || true
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure gdm3 || true
+sudo apt install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
+echo /usr/sbin/gdm3 > /etc/X11/default-display-manager
 
-  ## Install xrdp so Guacamole can connect via RDP
-  echo "init_vm.sh: xrdp"
-  sudo apt install -y xrdp xorgxrdp xfce4-session
-  sudo adduser xrdp ssl-cert
-  sudo -u "${VM_USER}" -i bash -c 'echo xfce4-session > ~/.xsession'
-  sudo -u "${VM_USER}" -i bash -c 'echo xset s off >> ~/.xsession'
-  sudo -u "${VM_USER}" -i bash -c 'echo xset -dpms >> ~/.xsession'
+## Install xrdp so Guacamole can connect via RDP
+echo "init_vm.sh: xrdp"
+sudo apt install -y xrdp xorgxrdp xfce4-session
+sudo adduser xrdp ssl-cert
+sudo -u "${VM_USER}" -i bash -c 'echo xfce4-session > ~/.xsession'
+sudo -u "${VM_USER}" -i bash -c 'echo xset s off >> ~/.xsession'
+sudo -u "${VM_USER}" -i bash -c 'echo xset -dpms >> ~/.xsession'
 
-  # Make sure xrdp service starts up with the system
-  sudo systemctl enable xrdp
-  sudo service xrdp restart
+# Make sure xrdp service starts up with the system
+sudo systemctl enable xrdp
+sudo service xrdp restart
 
-  ## Python 3.8 and Jupyter
-  sudo apt install -y jupyter-notebook microsoft-edge-dev
+## Python 3.8 and Jupyter
+sudo apt install -y jupyter-notebook microsoft-edge-dev
 
-  ## VS Code
-  echo "init_vm.sh: VS Code"
-  sudo apt install -y code
-  sudo apt install -y gvfs-bin || true
+## VS Code
+echo "init_vm.sh: VS Code"
+sudo apt install -y code
+sudo apt install -y gvfs-bin || true
 
-  echo "init_vm.sh: Folders"
-  sudo mkdir -p /opt/vscode/user-data
-  sudo mkdir -p /opt/vscode/extensions
+echo "init_vm.sh: Folders"
+sudo mkdir -p /opt/vscode/user-data
+sudo mkdir -p /opt/vscode/extensions
 
-  # echo "init_vm.sh: azure-cli"
-  sudo apt install azure-cli -y
+# echo "init_vm.sh: azure-cli"
+sudo apt install azure-cli -y
 
+# TODO: need to look at proxy extentions
+## VSCode Extensions
+# echo "init_vm.sh: VSCode extensions"
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension ms-python.python
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension REditorSupport.r
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension RDebugger.r-debugger
 
-  # Azure Storage Explorer
-  sudo apt-get remove -y dotnet-host-7.0
-  sudo apt-get remove -y dotnet-sdk-7.0
-  sudo apt-get install -y dotnet-sdk-8.0
-  sudo apt install gnome-keyring -y
+# Azure Storage Explorer
+sudo apt-get remove -y dotnet-host-7.0
+sudo apt-get remove -y dotnet-sdk-7.0
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt install gnome-keyring -y
 
-  sudo chmod 666 /etc/profile
+sudo chmod 666 /etc/profile
 
-  echo "export DOTNET_ROOT=/usr/share/dotnet
-  export PATH=$PATH:/usr/share/dotnet
-  " | sudo tee -a /etc/profile
+echo "export DOTNET_ROOT=/usr/share/dotnet
+export PATH=$PATH:/usr/share/dotnet
+" | sudo tee -a /etc/profile
 
-  sudo chmod 644 /etc/profile
+sudo chmod 644 /etc/profile
 
-  wget -q "${NEXUS_PROXY_URL}"/repository/microsoft-download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer-linux-x64.tar.gz -P /tmp
-  sudo mkdir /opt/storage-explorer
-  sudo tar xvf /tmp/StorageExplorer-linux-x64.tar.gz -C /opt/storage-explorer
-  sudo chmod +x /opt/storage-explorer/*
+wget -q "${NEXUS_PROXY_URL}"/repository/microsoft-download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer-linux-x64.tar.gz -P /tmp
+sudo mkdir /opt/storage-explorer
+sudo tar xvf /tmp/StorageExplorer-linux-x64.tar.gz -C /opt/storage-explorer
+sudo chmod +x /opt/storage-explorer/*
 
-  sudo tee /usr/share/applications/storage-explorer.desktop <<END
+sudo tee /usr/share/applications/storage-explorer.desktop << END
 [Desktop Entry]
 Name=Storage Explorer
 Comment=Azure Storage Explorer
@@ -83,32 +89,18 @@ StartupWMClass=Code
 Categories=Development;
 END
 
-  ## R
-  echo "init_vm.sh: R Setup"
-  sudo apt install -y r-base
+## R
+echo "init_vm.sh: R Setup"
+sudo apt install -y r-base
 
-  # RStudio Desktop
-  echo "init_vm.sh: RStudio"
-  wget "${NEXUS_PROXY_URL}"/repository/r-studio-download/electron/jammy/amd64/rstudio-2023.12.1-402-amd64.deb -P /tmp/2204
-  wget "${NEXUS_PROXY_URL}"/repository/r-studio-download/electron/focal/amd64/rstudio-2023.12.1-402-amd64.deb -P /tmp/2004
-  sudo gdebi --non-interactive /tmp/"${APT_SKU}"/rstudio-2023.12.1-402-amd64.deb
-
-  # Jupiter Notebook Config
-  sudo sed -i -e 's/Terminal=true/Terminal=false/g' /usr/share/applications/jupyter-notebook.desktop
-
-  # Default Browser
-  sudo update-alternatives --config x-www-browser
-
-fi
-
-# Update apt packages from configured Nexus sources
-sudo apt-get update
+# RStudio Desktop
+echo "init_vm.sh: RStudio"
+wget "${NEXUS_PROXY_URL}"/repository/r-studio-download/electron/jammy/amd64/rstudio-2023.12.1-402-amd64.deb -P /tmp/2204
+wget "${NEXUS_PROXY_URL}"/repository/r-studio-download/electron/focal/amd64/rstudio-2023.12.1-402-amd64.deb -P /tmp/2004
+sudo gdebi --non-interactive /tmp/"${APT_SKU}"/rstudio-2023.12.1-402-amd64.deb
 
 # Fix for blank screen on DSVM (/sh -> /bash due to conflict with profile.d scripts)
 sudo sed -i 's|!/bin/sh|!/bin/bash|g' /etc/xrdp/startwm.sh
-
-# Make sure xrdp service starts up with the system
-# sudo systemctl enable xrdp
 
 if [ "${SHARED_STORAGE_ACCESS}" -eq 1 ]; then
   # Install required packages
@@ -157,10 +149,10 @@ fi
 
 ### Anaconda Config
 if [ "${CONDA_CONFIG}" -eq 1 ]; then
-  export PATH="/opt/anaconda/condabin":$PATH
-  export PATH="/opt/anaconda/bin":$PATH
-  export PATH="/opt/anaconda/envs/py38_default/bin":$PATH
-  conda config
+  echo "init_vm.sh: Anaconda"
+  export PATH="/anaconda/condabin":$PATH
+  export PATH="/anaconda/bin":$PATH
+  export PATH="/anaconda/envs/py38_default/bin":$PATH
   conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda-mirror/main/  --system
   conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda-repo/main/  --system
   conda config --remove channels defaults --system
@@ -178,7 +170,13 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 # R config
-sudo echo -e "local({\n    r <- getOption(\"repos\")\n    r[\"Nexus\"] <- \"""${NEXUS_PROXY_URL}/repository/r-proxy/\"\n    options(repos = r)\n})" | sudo tee /etc/R/Rprofile.site
+sudo echo -e "local({\n    r <- getOption(\"repos\")\n    r[\"Nexus\"] <- \"""${NEXUS_PROXY_URL}\"/repository/r-proxy/\"\n    options(repos = r)\n})" | sudo tee /etc/R/Rprofile.site
+
+# Jupiter Notebook Config
+sudo sed -i -e 's/Terminal=true/Terminal=false/g' /usr/share/applications/jupyter-notebook.desktop
+
+# Default Browser
+sudo update-alternatives --config x-www-browser
 
 # Prevent screen timeout
 echo "init_vm.sh: Preventing Timeout"
