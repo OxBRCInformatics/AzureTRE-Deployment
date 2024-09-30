59c2950ee17ade54c16f987281ad1251f19ae290
Jaimie Withers <jaimie.withers@bdi.ox.ac.uk>
2024-09-30 16:59:11 +0000

    revert

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm-ouh2/terraform/vm_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm-ouh2/terraform/vm_config.sh
@@ -9,7 +9,6 @@ set -o xtrace
 # Remove apt sources not included in sources.list file
 sudo rm -f /etc/apt/sources.list.d/*
 
-
 # Update apt packages from configured Nexus sources
 echo "init_vm.sh: START"
 sudo apt update || true
@@ -36,41 +35,27 @@ sudo -u "${VM_USER}" -i bash -c 'echo xset -dpms >> ~/.xsession'
 sudo systemctl enable xrdp
 sudo service xrdp restart
 
-# Disable colord service if color management is not required
-sudo systemctl disable colord
-sudo systemctl stop colord
-
 ## Python 3.8 and Jupyter
 sudo apt install -y jupyter-notebook microsoft-edge-dev
 
 ## VS Code
+echo "init_vm.sh: VS Code"
+sudo apt install -y code
+sudo apt install -y gvfs-bin || true
+
 echo "init_vm.sh: Folders"
-sudo mkdir /opt/vscode/user-data
-sudo mkdir /opt/vscode/extensions
+sudo mkdir -p /opt/vscode/user-data
+sudo mkdir -p /opt/vscode/extensions
 
-echo "init_vm.sh: Keys"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install -y code gvfs-bin
-
-## VSCode Extensions
-echo "init_vm.sh: VSCode extensions"
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension ms-python.python
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension REditorSupport.r
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension RDebugger.r-debugger
-
-# Additional VS Code Extensions
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension njpwerner.autodocstring         # AutoDocstring - Python Docstring Generator
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension ms-python.data-wrangler      # Data Wrangler (Microsoft)
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension GrapeCity.gc-excelviewer      # Excel Viewer (Grape City)
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension formulahendry.auto-complete     # Path Autocomplete (Mihai Vilcu)
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension KevinRose.vsc-python-indent      # Python Indent (Kevin Rose)
-sudo code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension Gruntfuggly.todo-tree          # Todo Tree (Gruntfuggly)
-
-# Install Azure CLI
-echo "init_vm.sh: Installing Azure CLI"
+# echo "init_vm.sh: azure-cli"
 sudo apt install azure-cli -y
+
+# TODO: need to look at proxy extentions
+## VSCode Extensions
+# echo "init_vm.sh: VSCode extensions"
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension ms-python.python
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension REditorSupport.r
+# code --extensions-dir="/opt/vscode/extensions" --user-data-dir="/opt/vscode/user-data" --install-extension RDebugger.r-debugger
 
 # Azure Storage Explorer
 sudo apt-get remove -y dotnet-host-7.0
@@ -106,9 +91,6 @@ END
 
 ## R
 echo "init_vm.sh: R Setup"
-wget -q https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-sudo apt update
 sudo apt install -y r-base
 
 # RStudio Desktop
@@ -139,7 +121,7 @@ if [ "${SHARED_STORAGE_ACCESS}" -eq 1 ]; then
 
   # Create required file paths
   sudo mkdir -p "$mntPath"
-  sudo mkdir -p "$credentialRoot"
+  sudo mkdir -p "/etc/smbcredentials"
   sudo mkdir -p $mntRoot
 
   ### Auto FS to persist storage
@@ -164,56 +146,20 @@ if [ "${SHARED_STORAGE_ACCESS}" -eq 1 ]; then
   # Autofs mounts when accessed for 60 seconds.  Folder created for constant visible mount
   sudo ln -s "$mntPath" "/$fileShareName"
 
-  # Ensure permissions are set correctly on the mount point
   sudo chmod 777 "$mntPath"
 fi
-
-# Install LibreOffice
-echo "init_vm.sh: Installing LibreOffice"
-sudo apt install -y libreoffice
-
-# Anaconda
-echo "init_vm.sh: Anaconda"
-sudo apt -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh -P /tmp
-chmod +x /tmp/Anaconda3-2022.05-Linux-x86_64.sh
-bash /tmp/Anaconda3-2022.05-Linux-x86_64.sh -b -p /opt/anaconda
-/opt/anaconda/bin/conda install -y -c anaconda anaconda-navigator
 
 ### Anaconda Config
 if [ "${CONDA_CONFIG}" -eq 1 ]; then
   echo "init_vm.sh: Anaconda"
-  export PATH="/opt/anaconda/condabin":$PATH
-  export PATH="/opt/anaconda/bin":$PATH
-  export PATH="/opt/anaconda/envs/py38_default/bin":$PATH
+  export PATH="/anaconda/condabin":$PATH
+  export PATH="/anaconda/bin":$PATH
+  export PATH="/anaconda/envs/py38_default/bin":$PATH
   conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda-mirror/main/  --system
   conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda-repo/main/  --system
   conda config --remove channels defaults --system
   conda config --set channel_alias "${NEXUS_PROXY_URL}"/repository/conda-mirror/  --system
 fi
-
-## JDK
-sudo apt install -y openjdk-8-jdk
-
-## ProM Tools
-wget http://promtools.org/prom6/downloads/prom-6.12-all-platforms.tar.gz -P /tmp
-sudo mkdir /opt/prom-tools
-tar -xf /tmp/prom-6.12-all-platforms.tar.gz -C /opt/prom-tools
-sudo chmod +x /opt/prom-tools/*.sh
-
-## PostgreSQL
-# Create the file repository configuration:
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-
-# Import the repository signing key:
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-# Update the package lists:
-sudo apt-get update
-
-# Install the latest version of PostgreSQL.
-# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-sudo apt-get -y install postgresql
 
 # Docker install and config
 sudo apt-get remove -y moby-tini || true
@@ -237,12 +183,6 @@ sudo update-alternatives --config x-www-browser
 # Prevent screen timeout
 echo "init_vm.sh: Preventing Timeout"
 sudo apt-get remove xfce4-screensaver -y
-sudo apt-get remove -y light-locker
-
-xfconf-query -c xfce4-screensaver -p /active -s false
-xfconf-query -c xfce4-session -p /LockScreen -s false
-xfconf-query -c xfce4-session -p /Session/Idle -s 0
-
 
 ## Cleanup
 echo "init_vm.sh: Cleanup"
